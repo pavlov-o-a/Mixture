13d0f6d434e2f4d204f72b8157dbfe3558a11ee2
oleg_pavlov <forpoststuff@gmail.com>
2021-03-23 17:48:16 +0300

    set theme in app. added links in profile.

--- profile/src/main/java/com/mixture/profile/ProfileFragment.kt
+++ profile/src/main/java/com/mixture/profile/ProfileFragment.kt
@@ -1,12 +1,25 @@
 package com.mixture.profile
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
+import android.text.SpannableStringBuilder
+import android.text.style.ClickableSpan
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.mixture.profile.databinding.FragmentProfileBinding
+
+private const val GMAIL_URL = "mailto:forpoststuff@gmail.com"
+private const val LINKEDIN_URL = "https://www.linkedin.com/in/pavlov-oleg/"
+private const val GIT_URL = "https://github.com/pavlov-o-a"
+private const val DYNAMIC_FEATURE_LINK =
+    "https://developer.android.com/guide/app-bundle/play-feature-delivery"
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
     lateinit var viewBinder: FragmentProfileBinding
@@ -19,16 +32,51 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
                 ctx.setSupportActionBar(viewBinder.profileToolbar)
             }
         }
-        viewBinder.profileToolbar.setNavigationIcon(R.drawable.ic_linkedin)
+        setHasOptionsMenu(true)
         viewBinder.profileToolbar.setNavigationOnClickListener {
-            Navigation.findNavController(view).navigateUp()
+            Navigation.findNavController(requireView()).navigateUp()
+        }
+        viewBinder.gmailLink.setOnClickListener {
+            sendIntentSafely(Intent(Intent.ACTION_SENDTO, Uri.parse(GMAIL_URL)))
+        }
+        viewBinder.linkedinLink.setOnClickListener {
+            sendIntentSafely(Intent(Intent.ACTION_VIEW, Uri.parse(LINKEDIN_URL)))
+        }
+        viewBinder.gitLink.setOnClickListener {
+            sendIntentSafely(Intent(Intent.ACTION_VIEW, Uri.parse(GIT_URL)))
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        view?.let {
-            Navigation.findNavController(it).popBackStack()
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(com.mixture.common.R.menu.info_menu, menu)
+        menu.findItem(com.mixture.common.R.id.menu_info).setOnMenuItemClickListener {
+            val linkSpan = object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    sendIntentSafely(Intent(Intent.ACTION_VIEW, Uri.parse(DYNAMIC_FEATURE_LINK)))
+                }
+            }
+            val message = SpannableStringBuilder(getString(R.string.profile_info)).apply {
+                setSpan(linkSpan, 17, 31, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+            AlertDialog.Builder(requireContext())
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.dismiss() }
+                .show()
+            true
         }
-        return true
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun sendIntentSafely(intent: Intent) {
+        try {
+            startActivity(intent)
+        } catch (exc: Exception) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.profile_link_error),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
